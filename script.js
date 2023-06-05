84978507a55f9fc6bab1f7ef2f87fe698d7c6499
Ismael Cruz <auriazul.021@gmail.com>
2023-06-04 18:15:17 -0600

    Se añadió la función de validar al btbEncriptar

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ const mensajeEncriptado = document.querySelector(".mensajeEncriptado");
 const copia = document.querySelector(".btnCopiar");
 copia.style.display = "none"
 
+function validar(){
+    let mensaje = document.querySelector(".mensaje").value;
+    let validador = mensaje.match(/^[a-z]*$/)
+
+    if(!validador || validador === 0) {
+        alert("Solo se permiten letras minúsculas y sin acento");
+        location.reload();
+        return true;
+    }
+}
+
 function copiar(){
     mensaje.select();
     navigator.clipboard.writeText(mensaje.value)
